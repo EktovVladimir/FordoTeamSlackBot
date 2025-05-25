9d188598fa7282461e360c8daa342c71275e8c3c
Ektov Vladimir <ektov@mego.travel>
2025-05-25 13:24:25 +0300

    - unused code

--- internal/models/review/request.go
+++ internal/models/review/request.go
@@ -11,31 +11,3 @@ type Request struct {
 	pullRequests []github.PullRequest
 	randomCount  int
 }
-
-func (r *Request) Requester() slack.User {
-	return r.requester
-}
-
-func (r *Request) Reviewers() []slack.User {
-	return r.reviewers
-}
-
-func (r *Request) PullRequests() []github.PullRequest {
-	return r.pullRequests
-}
-
-func (r *Request) SetRandomReviewersCount(randomCount int) {
-	r.randomCount = randomCount
-}
-
-func (r *Request) SetReviewers(reviewers []slack.User) {
-	r.reviewers = reviewers
-}
-
-func NewRequest(requester slack.User, prs []github.PullRequest) *Request {
-	return &Request{
-		requester:    requester,
-		pullRequests: prs,
-		reviewers:    make([]slack.User, 0),
-	}
-}
