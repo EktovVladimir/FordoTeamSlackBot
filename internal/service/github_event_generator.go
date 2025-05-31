8bbd273abb79e886a1fdc0cb3870ac33016d9d63
Ektov Vladimir <ektov@mego.travel>
2025-05-31 20:34:27 +0300

    = done app by context

--- internal/service/github_event_generator.go
+++ internal/service/github_event_generator.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"fmt"
 	"github.com/EktovVladimir/FordoTeamSlackBot/internal/models/event"
 	"github.com/EktovVladimir/FordoTeamSlackBot/internal/repository"
@@ -19,18 +20,28 @@ func NewGithubEventGenerator(count int, intervalMs int) *githubEventGenerator {
 	}
 }
 
-func (s *githubEventGenerator) Start() <-chan repository.Event {
+func (s *githubEventGenerator) Start(ctx context.Context) <-chan repository.Event {
 	ch := make(chan repository.Event)
 	go func() {
-		for i := 0; i < s.count; i++ {
-			ev := event.GenerateRandomPullRequestEvent()
-			ch <- ev
-			time.Sleep(time.Duration(s.intervalMs) * time.Millisecond)
+		defer close(ch)
+		defer fmt.Println("githubEventGenerator: finished")
+
+		counter := 0
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("githubEventGenerator: context done")
+				return
+			case <-time.After(time.Duration(s.intervalMs) * time.Millisecond):
+				if s.count > 0 && counter >= s.count {
+					return
+				}
+
+				ev := event.GenerateRandomPullRequestEvent()
+				ch <- ev
+				counter++
+			}
 		}
-
-		fmt.Println("githubSlackGenerator finished")
-
-		close(ch)
 	}()
 
 	return ch
