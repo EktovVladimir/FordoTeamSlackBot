8a327d946968cdeaf96a2aa5d0aa477c621a42a9
Ektov Vladimir <ektov@mego.travel>
2025-06-15 14:24:15 +0300

    + config & logs infra

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/EktovVladimir/FordoTeamSlackBot
 
 go 1.23.4
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/heetch/confita v0.10.0
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/lestrrat-go/strftime v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
---
+++ internal/shared/config/config.go
@@ -0,0 +1,67 @@
+package config
+
+import (
+	"context"
+	"github.com/EktovVladimir/FordoTeamSlackBot/internal/shared/environment"
+	"github.com/heetch/confita"
+	"github.com/heetch/confita/backend/env"
+	"github.com/heetch/confita/backend/file"
+	"github.com/heetch/confita/backend/flags"
+	"path/filepath"
+)
+
+type Config struct {
+	Log       LogConfig       `config:"log"`
+	CrudApi   CrudApiConfig   `config:"crudApi"`
+	JsonStore JsonStoreConfig `config:"jsonStore"`
+}
+
+type LogConfig struct {
+	Level  string `config:"log-level"`
+	Dir    string `config:"log-dir"`
+	MaxAge int    `config:"log-maxAge"`
+}
+
+type CrudApiConfig struct {
+	Host string `config:"crudApi-host"`
+	Port int    `config:"crudApi-port"`
+}
+
+type JsonStoreConfig struct {
+	Path string `config:"jsonStore-path"`
+}
+
+func Load() *Config {
+	configPath := filepath.Join("configs", "app."+environment.Env+".json")
+
+	loader := confita.NewLoader(
+		file.NewBackend(configPath),
+		flags.NewBackend(),
+		env.NewBackend())
+
+	cfg := getDefaultConfig()
+
+	if err := loader.Load(context.Background(), cfg); err != nil {
+		//Очень не ожидаем получить тут ошибку, но если получили, то совсем всё плохо
+		panic(err)
+	}
+
+	return cfg
+}
+
+func getDefaultConfig() *Config {
+	return &Config{
+		Log: LogConfig{
+			Level:  "info",
+			Dir:    "./logs",
+			MaxAge: 90,
+		},
+		CrudApi: CrudApiConfig{
+			Host: "localhost",
+			Port: 8000,
+		},
+		JsonStore: JsonStoreConfig{
+			Path: "./data",
+		},
+	}
+}
